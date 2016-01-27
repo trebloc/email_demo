0bb84d605c851965d93fe5d4881c3ebd26165aed
trebloc <ctse77@alum.berkeley.edu>
2016-01-26 19:27:30 -0800

    lighning talks assignment

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
-# ActionMailer::Base.smtp_settings = {
-#   :user_name => 'your_sendgrid_username',
-#   :password => 'your_sendgrid_password',
-#   :domain => 'yourdomain.com',
-#   :address => 'smtp.sendgrid.net',
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-# }
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'thenathanator',
+  :password => 'Cy8saY8rYob2kE',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
